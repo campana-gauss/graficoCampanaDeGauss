7c179ee52b1e6d9eeae7368c0782b6da10697bf9
dxn1l <danielmorenorey2012@gmail.com>
2024-10-11 18:27:59 +0200

    Added Spring initializer

---
+++ src/main/java/com/example/graficoCampanaDeGauss/GraficoCampanaDeGaussApplication.java
@@ -0,0 +1,13 @@
+package com.example.graficoCampanaDeGauss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraficoCampanaDeGaussApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraficoCampanaDeGaussApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/graficoCampanaDeGauss/GraficoCampanaDeGaussApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graficoCampanaDeGauss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraficoCampanaDeGaussApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
